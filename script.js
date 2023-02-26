4849929ea677852583b92c3a367993e22ed7879b
Preston C <pchandler858@gmail.com>
2023-02-26 15:23:20 -0800

    added save to localStorage funtionality

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ var listUl = document.querySelector("ul");
 var listItems = document.querySelectorAll("li");
 var lis = listUl.children;
 
+function populateList() {
+  var itemsArray = JSON.parse(localStorage.getItem("itemsArray")) || [];
+  for (var i = 0; i < itemsArray.length; i++) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(itemsArray[i]));
+    li.addEventListener("click", toggleDone);
+    listUl.appendChild(li);
+  }
+}
+
+populateList();
+
 function addDeleteButton(li) {
   let deleteBtn = document.createElement("button");
   deleteBtn.className = "delete";
-  deleteBtn.id = "delete";
   deleteBtn.textContent = "Delete";
+  deleteBtn.setAttribute("data-item", li.innerText);
   li.appendChild(deleteBtn);
 }
 
@@ -16,9 +28,13 @@ for (let i = 0; i < lis.length; i++) {
   addDeleteButton(lis[i]);
 }
 
-function toggleDone(li) {
-  this.classList.toggle("done");
+function toggleDone(event) {
+  // Check if the event target is the button or the text node inside the li element
+  if (event.target.tagName !== "BUTTON") {
+    event.target.classList.toggle("done");
+  }
 }
+
 
 for (let i = 0; i < lis.length; i++) {
   lis[i].addEventListener("click", toggleDone);
@@ -42,6 +58,15 @@ function createListElement() {
   addDeleteButton(li);
   ul.appendChild(li);
   input.value = "";
+
+  // Save the list item to local storage if it's not empty
+  if (li.innerText.trim() !== "") {
+    // Remove the "Delete" button text from the li element
+    var itemText = li.innerText.replace("Delete", "").trim();
+    var itemsArray = JSON.parse(localStorage.getItem("itemsArray")) || [];
+    itemsArray.push(itemText);
+    localStorage.setItem("itemsArray", JSON.stringify(itemsArray));
+  }
 }
 
 function addListAfterClick() {
@@ -56,6 +81,15 @@ function deleteItem(event) {
       let li = event.target.parentNode;
       let ul = li.parentNode;
       ul.removeChild(li);
+
+      // Remove the item from local storage
+      var itemsArray = JSON.parse(localStorage.getItem("itemsArray")) || [];
+      var itemText = event.target.getAttribute("data-item");
+      var itemIndex = itemsArray.indexOf(itemText);
+      if (itemIndex !== -1) {
+        itemsArray.splice(itemIndex, 1);
+        localStorage.setItem("itemsArray", JSON.stringify(itemsArray));
+      }
     }
   }
 }
@@ -63,3 +97,5 @@ function deleteItem(event) {
 listUl.addEventListener("click", deleteItem);
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
+li.addEventListener("click", toggleDone);
+
